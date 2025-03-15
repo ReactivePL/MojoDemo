b0a6b749889b6bdf6f8183390f12046cde1412a0
Robert Moore <robert@r-moore.tech>
2025-03-15 19:57:25 +0000

    add additional example

---
+++ lib/ReactivePL/Reactive/Components/Input.pm
@@ -0,0 +1,23 @@
+package ReactivePL::Reactive::Components::Input;
+
+use warnings;
+use strict;
+
+use Moo;
+use namespace::clean;
+use Types::Standard qw( Str Int Enum ArrayRef Object );
+
+use Data::Printer;
+
+has name => (is => 'ro', isa => Str, required => 1);
+has value => (is => 'rw', isa => Str, default => sub { return '' });
+
+sub render {
+    my $self = shift;
+
+    return <<'HTML';
+        <input class="custom-input-component" type="text" name="<%= $name %>" reactive:model="value"/>
+HTML
+}
+
+1;
